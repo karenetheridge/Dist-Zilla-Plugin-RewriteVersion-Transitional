a0ba466257741c0f39dd51cc8b7c9f7fb4b972f9
Karen Etheridge <ether@cpan.org>
2015-01-18 19:50:43 -0800

    After release: bump $VERSION

--- lib/Dist/Zilla/Plugin/BumpVersionAfterRelease/Transitional.pm
+++ lib/Dist/Zilla/Plugin/BumpVersionAfterRelease/Transitional.pm
@@ -5,7 +5,7 @@ package Dist::Zilla::Plugin::BumpVersionAfterRelease::Transitional;
 # KEYWORDS: plugin version rewrite munge module
 # vim: set ts=8 sw=4 tw=78 et :
 
-our $VERSION = '0.004';
+our $VERSION = '0.005';
 use Moose;
 extends 'Dist::Zilla::Plugin::BumpVersionAfterRelease';
 with 'Dist::Zilla::Role::InsertVersion';
