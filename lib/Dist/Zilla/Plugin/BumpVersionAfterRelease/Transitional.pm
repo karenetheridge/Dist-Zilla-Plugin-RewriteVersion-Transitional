7f50647edd15eff8d29c9aa9a6f7177d87a4cb78
Karen Etheridge <ether@cpan.org>
2015-05-19 21:20:13 -0700

    capitalize the abstract

--- lib/Dist/Zilla/Plugin/BumpVersionAfterRelease/Transitional.pm
+++ lib/Dist/Zilla/Plugin/BumpVersionAfterRelease/Transitional.pm
@@ -1,7 +1,7 @@
 use strict;
 use warnings;
 package Dist::Zilla::Plugin::BumpVersionAfterRelease::Transitional;
-# ABSTRACT: ease the transition to [BumpVersionAfterRelease] in your distribution
+# ABSTRACT: Ease the transition to [BumpVersionAfterRelease] in your distribution
 # KEYWORDS: plugin version rewrite munge module
 # vim: set ts=8 sts=4 sw=4 tw=78 et :
 
--- lib/Dist/Zilla/Plugin/RewriteVersion/Transitional.pm
+++ lib/Dist/Zilla/Plugin/RewriteVersion/Transitional.pm
@@ -1,7 +1,7 @@
 use strict;
 use warnings;
 package Dist::Zilla::Plugin::RewriteVersion::Transitional;
-# ABSTRACT: ease the transition to [RewriteVersion] in your distribution
+# ABSTRACT: Ease the transition to [RewriteVersion] in your distribution
 # KEYWORDS: plugin version rewrite munge module
 # vim: set ts=8 sts=4 sw=4 tw=78 et :
 
