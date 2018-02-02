a958a540e6c7da3b26c62374acfc62c6187bcaa6
Karen Etheridge <ether@cpan.org>
2018-02-01 18:45:31 -0800

    increment $VERSION after 0.009 release

--- lib/Dist/Zilla/Role/InsertVersion.pm
+++ lib/Dist/Zilla/Role/InsertVersion.pm
@@ -4,7 +4,7 @@ package # hide from PAUSE
     Dist::Zilla::Role::InsertVersion;
 # vim: set ts=8 sts=4 sw=4 tw=115 et :
 
-our $VERSION = '0.009';
+our $VERSION = '0.010';
 
 use Moose::Role;
 use Scalar::Util 'blessed';
